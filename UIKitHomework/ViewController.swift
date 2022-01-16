a5bf3cdcb21f6fe94e1a158765b8bd2409a74cb1
Arsalan Balzhinimaev <arsalashka@gmail.com>
2022-01-16 23:53:50 +0300

    Initial Commit

---
+++ UIKitHomework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitHomework
+//
+//  Created by Arsalan Balzhinimaev on 16.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
