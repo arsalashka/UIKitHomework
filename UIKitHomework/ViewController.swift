3014cc24611f7c6c3c19915f84fb6fd381477581
Arsalan Balzhinimaev <arsalashka@gmail.com>
2022-01-25 22:28:14 +0300

    resetup constraints

--- UIKitHomework/ViewController.swift
+++ UIKitHomework/ViewController.swift
@@ -9,10 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet var mainView: UIView!
 
+	@IBOutlet var redLabel: UILabel!
+	@IBOutlet var greenLabel: UILabel!
+	@IBOutlet var blueLabel: UILabel!
+
+	@IBOutlet var redSlider: UISlider!
+	@IBOutlet var greenSlider: UISlider!
+	@IBOutlet var blueSlider: UISlider!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+
+		// Sliders
+		redSlider.minimumTrackTintColor = .red
+
+
+
+		// Labels
+
+
+
 
 
 	}
